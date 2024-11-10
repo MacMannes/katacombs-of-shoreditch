5d041c20e6682430cf10c1acb71c69acb0206185
André Mathlener <info@macmannes.nl>
2024-11-10 20:08:55 +0100

    Refactor test

--- src/katas/katacombs/domain/repository/room-repository.test.ts
+++ src/katas/katacombs/domain/repository/room-repository.test.ts
@@ -3,41 +3,47 @@ import { Room, RoomRepository } from '@katas/katacombs/domain';
 
 describe('RoomRepository', () => {
     describe('Create new Repository', () => {
-        it('should not allow two rooms with the same name', () => {
-            const room = new Room('start', '', '');
-            expect(() => new RoomRepository([room, room])).toThrowError();
+        describe('Room Validation', () => {
+            it('should not allow two rooms with the same name', () => {
+                const room = new Room('start', '', '');
+                expect(() => new RoomRepository([room, room])).toThrowError();
+            });
+
+            it('should not allow two rooms with the same title', () => {
+                const rooms = [new Room('room1', 'Room', ''), new Room('room2', 'Room', '')];
+                expect(() => new RoomRepository(rooms)).toThrowError();
+            });
+
+            it('should validate if a room with the name start exists', () => {
+                const room1 = new Room('room1', 'Room 1', '');
+                const room2 = new Room('room2', 'Room 2', '');
+
+                const rooms = [room1, room2];
+                expect(() => new RoomRepository(rooms)).toThrowError('A room with the name "start" does not exist.');
+            });
         });
 
-        it('should not allow two rooms with the same title', () => {
-            const rooms = [new Room('room1', 'Room', ''), new Room('room2', 'Room', '')];
-            expect(() => new RoomRepository(rooms)).toThrowError();
-        });
+        describe('Connection Validation', () => {
+            it('should not allow non-traversable connections to rooms', () => {
+                const room1 = new Room('start', 'Room 1', '');
+                const room2 = new Room('room2', 'Room 2', '');
+                room1.addConnection('north', room2);
 
-        it('should not allow non-traversable connections to rooms', () => {
-            const room1 = new Room('start', 'Room 1', '');
-            const room2 = new Room('room2', 'Room 2', '');
-            room1.addConnection('north', room2);
+                const rooms = [room1, room2];
+                expect(() => new RoomRepository(rooms)).toThrowError(
+                    'The connection from start to room2 is not reversed.',
+                );
+            });
 
-            const rooms = [room1, room2];
-            expect(() => new RoomRepository(rooms)).toThrowError('The connection from start to room2 is not reversed.');
-        });
+            it('should not fail when all connections are reversed', () => {
+                const room1 = new Room('start', 'Room 1', '');
+                const room2 = new Room('room2', 'Room 2', '');
+                room1.addConnection('south', room2);
+                room2.addConnection('north', room1);
 
-        it('should not fail when all connections are reversed', () => {
-            const room1 = new Room('start', 'Room 1', '');
-            const room2 = new Room('room2', 'Room 2', '');
-            room1.addConnection('south', room2);
-            room2.addConnection('north', room1);
-
-            const rooms = [room1, room2];
-            expect(() => new RoomRepository(rooms)).not.toThrowError();
-        });
-
-        it('should validate if a room with the name start exists', () => {
-            const room1 = new Room('room1', 'Room 1', '');
-            const room2 = new Room('room2', 'Room 2', '');
-
-            const rooms = [room1, room2];
-            expect(() => new RoomRepository(rooms)).toThrowError('A room with the name "start" does not exist.');
+                const rooms = [room1, room2];
+                expect(() => new RoomRepository(rooms)).not.toThrowError();
+            });
         });
     });
 });
