084c73cbd7519f667b72f52c02dfe6c630be2f4d
André Mathlener <andre.mathlener@ns.nl>
2024-11-20 21:14:40 +0100

    Added setWindowTitle

--- src/katas/katacombs/ui/default-user-interface.ts
+++ src/katas/katacombs/ui/default-user-interface.ts
@@ -2,15 +2,24 @@ import { Room } from '../domain';
 import { UserInterface } from '@katas/katacombs/ui';
 
 export class DefaultUserInterface implements UserInterface {
-    displayRoom(room: Room): void {
+    public displayRoom(room: Room): void {
         console.log(room.description);
+        this.setWindowTitle(room.title);
     }
 
-    displayMessage(message: string): void {
+    public displayMessage(message: string): void {
         console.log(message);
     }
 
-    getUserInput(): Promise<string | undefined> {
+    public getUserInput(): Promise<string | undefined> {
         throw new Error('Method not implemented.');
+    }
+
+    private setWindowTitle(title: string) {
+        if (process.platform == 'win32') {
+            process.title = title;
+        } else {
+            console.log('\x1b]2;' + title + '\x1b\x5c');
+        }
     }
 }
