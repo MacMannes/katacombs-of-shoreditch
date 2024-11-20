a91f8652935fbf71f9ceea8bc500eaa89b2d4a4b
André Mathlener <andre.mathlener@ns.nl>
2024-11-20 21:25:44 +0100

    Added app

---
+++ src/katas/katacombs/app.ts
@@ -0,0 +1,12 @@
+import { DefaultUserInterface } from '@katas/katacombs/ui';
+import { GameController } from '@katas/katacombs/game-controller';
+import { createTestRooms, Game, ItemRepository, RoomRepository } from '@katas/katacombs/domain';
+
+const ui = new DefaultUserInterface();
+const testRooms = createTestRooms();
+const roomRepository = new RoomRepository(testRooms);
+const itemRepository = new ItemRepository();
+const game = new Game(roomRepository, itemRepository);
+const controller = new GameController(game, ui);
+
+await controller.startGame();
