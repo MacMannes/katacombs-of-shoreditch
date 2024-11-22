f41a3f08daede2a3767e2b895ee8d33c34c392f6
André Mathlener <andre.mathlener@ns.nl>
2024-11-22 09:52:04 +0100

    Add Responses to CommandAction

--- src/katas/katacombs/domain/model/command-action.ts
+++ src/katas/katacombs/domain/model/command-action.ts
@@ -2,4 +2,10 @@ export type CommandAction = {
     command: string;
     argument: string;
     parameter?: string;
+    responses?: Responses;
+};
+
+type Responses = {
+    success?: string;
+    failure?: string;
 };
