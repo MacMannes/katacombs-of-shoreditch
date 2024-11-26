731334a7ee0a7e387270ed876618495612174ceb
André Mathlener <andre.mathlener@ns.nl>
2024-11-26 15:39:40 +0100

    Use GameFactory in app

--- src/katas/katacombs/app.ts
+++ src/katas/katacombs/app.ts
@@ -1,13 +1,18 @@
 import { DefaultUserInterface } from '@katas/katacombs/ui';
 import { GameController } from '@katas/katacombs/game-controller';
-import { createTestRooms, Game, ItemRepository, RoomRepository } from '@katas/katacombs/domain';
+import { GameFactory, YamlDataLoader } from '@katas/katacombs/domain';
+import { fileURLToPath } from 'node:url';
+import { dirname } from 'path';
+import path from 'node:path';
 
-const ui = new DefaultUserInterface();
-const testRooms = await createTestRooms();
-const roomRepository = new RoomRepository(testRooms);
-const itemRepository = new ItemRepository();
-const game = new Game(roomRepository, itemRepository);
-const controller = new GameController(game, ui);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const gameFactory = new GameFactory(new YamlDataLoader());
+const gameDataPath = path.resolve(__dirname, './resources/test-game-data.yaml');
+const game = await gameFactory.createGame(gameDataPath);
+
+const controller = new GameController(game, new DefaultUserInterface());
 
 await controller.startGame();
 process.exit(0);
