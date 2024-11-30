920e0a3245ead93cb2dd235aa4c506b4b6a32799
André Mathlener <info@macmannes.nl>
2024-11-30 16:16:30 +0100

    Rename parameter

--- src/katas/katacombs/ui/user-interface.ts
+++ src/katas/katacombs/ui/user-interface.ts
@@ -3,6 +3,6 @@ import { Room } from '@katas/katacombs/domain/model';
 export type UserInterface = {
     displayWelcomeMessage(): Promise<void>;
     displayRoom(room: Room): Promise<void>;
-    displayMessage(message: string, audioKeys?: string[]): Promise<void>;
+    displayMessage(message: string, audioFiles?: string[]): Promise<void>;
     getUserInput(): Promise<string | undefined>;
 };
