54aac2a30f7c5dbe33402757018e5d7eb3fb28d9
André Mathlener <info@macmannes.nl>
2024-11-30 16:41:10 +0100

    Implemented AudioPlayer

--- src/katas/katacombs/ui/default-audio-player.ts
+++ src/katas/katacombs/ui/default-audio-player.ts
@@ -1,7 +1,15 @@
 import { AudioPlayer } from '@katas/katacombs/ui/audio-player';
+import path from 'node:path';
+import play_sound from 'play-sound';
+import { fileURLToPath } from 'node:url';
+
+const player = play_sound();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export class DefaultAudioPlayer implements AudioPlayer {
     public async play(fileName: string): Promise<void> {
-        return Promise.resolve();
+        const filePath = path.join(__dirname, `../resources/sounds/${fileName}.mp3`);
+        player.play(filePath);
     }
 }
