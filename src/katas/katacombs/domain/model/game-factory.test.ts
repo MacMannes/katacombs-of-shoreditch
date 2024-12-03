9a725da3e45ac74c7a0a934e8352c0adf135be86
André Mathlener <info@macmannes.nl>
2024-12-03 20:43:12 +0100

    Fixed GameFactory tests

--- src/katas/katacombs/domain/model/game-factory.test.ts
+++ src/katas/katacombs/domain/model/game-factory.test.ts
@@ -1,6 +1,6 @@
 import { describe, expect, it } from 'vitest';
 import { createMockedObject } from '@utils/test';
-import { GameFactory, Room, YamlDataLoader } from '@katas/katacombs/domain';
+import { GameFactory, GameRealm, Room, YamlDataLoader } from '@katas/katacombs/domain';
 
 function createRooms(): Room[] {
     return [new Room('start', 'Beginning', 'You are at the beginning of the game.')];
@@ -8,7 +8,8 @@ function createRooms(): Room[] {
 
 describe('GameFactory', () => {
     const dataLoader = createMockedObject(YamlDataLoader);
-    dataLoader.load.mockResolvedValue(createRooms());
+    const realm: GameRealm = { rooms: createRooms(), texts: {} };
+    dataLoader.load.mockResolvedValue(realm);
 
     it('should create a new game with rooms from the specified path', async () => {
         const gameFactory = new GameFactory(dataLoader);
