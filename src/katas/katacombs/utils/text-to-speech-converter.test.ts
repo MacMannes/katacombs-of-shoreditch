f88d8b749547a692c23725fd7863af2a42d6ca74
André Mathlener <info@macmannes.nl>
2024-12-05 21:50:07 +0100

    Only convert texts that aren't converted yet

--- src/katas/katacombs/utils/text-to-speech-converter.test.ts
+++ src/katas/katacombs/utils/text-to-speech-converter.test.ts
@@ -1,13 +1,25 @@
-import { describe, expect, it } from 'vitest';
+import { afterEach, describe, expect, it, vi } from 'vitest';
 import { TextToSpeechConverter } from '@katas/katacombs/utils/text-to-speech-converter';
 import { createMockedObject } from '@utils/test';
 import { TextToSpeachService } from '@katas/katacombs/utils/text-to-speach-service';
+import { existsSync } from 'node:fs';
+
+vi.mock('fs', () => ({
+    existsSync: vi.fn(),
+}));
+const existsSyncMock = vi.mocked(existsSync);
 
 describe('Text to Speech Converter', () => {
     const service = createMockedObject(TextToSpeachService);
     const converter = new TextToSpeechConverter(service);
 
-    it('should call the TextToSpeechService', async () => {
+    afterEach(() => {
+        vi.resetAllMocks();
+    });
+
+    it('should call the TextToSpeechService for each text', async () => {
+        existsSyncMock.mockReturnValue(false);
+
         const texts: Record<string, string> = {
             'msg-welcome': 'Welcome to Katacombs of Shoreditch',
             'msg-hello-world': 'Hello World!',
@@ -15,5 +27,18 @@ describe('Text to Speech Converter', () => {
 
         await converter.convert(texts);
         expect(service.convert).toBeCalledTimes(2);
+    });
+
+    it('should only call the TextToSpeechService for msg-welcome', async () => {
+        existsSyncMock.mockImplementation((path) => path !== 'msg-welcome');
+
+        const texts: Record<string, string> = {
+            'msg-welcome': 'Welcome to Katacombs of Shoreditch',
+            'msg-hello-world': 'Hello World!',
+        };
+
+        await converter.convert(texts);
+        expect(service.convert).toBeCalledTimes(1);
+        expect(service.convert).toBeCalledWith('Welcome to Katacombs of Shoreditch', 'msg-welcome');
     });
 });
--- src/katas/katacombs/utils/text-to-speech-converter.ts
+++ src/katas/katacombs/utils/text-to-speech-converter.ts
@@ -1,11 +1,14 @@
 import { TextToSpeachService } from '@katas/katacombs/utils/text-to-speach-service';
+import { existsSync } from 'node:fs';
 
 export class TextToSpeechConverter {
     constructor(private readonly service: TextToSpeachService) {}
 
     public async convert(texts: Record<string, string>): Promise<void> {
         for (const [fileName, text] of Object.entries(texts)) {
-            await this.service.convert(text, fileName);
+            if (!existsSync(fileName)) {
+                await this.service.convert(text, fileName);
+            }
         }
     }
 }
