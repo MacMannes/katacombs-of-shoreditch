ba781e05df50c8604f60c5918f93c109377fde94
André Mathlener <info@macmannes.nl>
2024-12-06 21:15:27 +0100

    Use absolute paths

--- src/katas/katacombs/utils/text-to-speech-converter.test.ts
+++ src/katas/katacombs/utils/text-to-speech-converter.test.ts
@@ -2,6 +2,8 @@ import { afterEach, describe, expect, it, vi } from 'vitest';
 import { TextToSpeechConverter, TextToSpeechService } from '@katas/katacombs/utils';
 import { createMockedObject } from '@utils/test';
 import { existsSync } from 'node:fs';
+import { getAbsolutePath, RESOURCES_PATH } from '@katas/katacombs/paths';
+import path from 'node:path';
 
 vi.mock('fs', () => ({
     existsSync: vi.fn(),
@@ -28,8 +30,35 @@ describe('Text to Speech Converter', () => {
         expect(service.convert).toBeCalledTimes(2);
     });
 
+    it('should add .mp3 to the fileName', async () => {
+        existsSyncMock.mockReturnValue(false);
+
+        const texts: Record<string, string> = {
+            'msg-welcome': 'Welcome to Katacombs of Shoreditch',
+            'msg-hello-world': 'Hello World!',
+        };
+
+        await converter.convert(texts);
+        expect(service.convert).toBeCalledTimes(2);
+        expect(service.convert).toBeCalledWith(expect.anything(), expect.stringContaining('.mp3'));
+    });
+
+    it('should use absolute paths', async () => {
+        existsSyncMock.mockReturnValue(false);
+        const pathPrefix = getAbsolutePath(path.join(RESOURCES_PATH, 'audio'));
+
+        const texts: Record<string, string> = {
+            'msg-welcome': 'Welcome to Katacombs of Shoreditch',
+            'msg-hello-world': 'Hello World!',
+        };
+
+        await converter.convert(texts);
+        expect(service.convert).toBeCalledTimes(2);
+        expect(service.convert).toBeCalledWith(expect.anything(), expect.stringContaining(pathPrefix));
+    });
+
     it('should only call the TextToSpeechService for msg-welcome', async () => {
-        existsSyncMock.mockImplementation((path) => path !== 'msg-welcome');
+        existsSyncMock.mockImplementation((path) => !path.toString().includes('msg-welcome'));
 
         const texts: Record<string, string> = {
             'msg-welcome': 'Welcome to Katacombs of Shoreditch',
--- src/katas/katacombs/utils/text-to-speech-converter.ts
+++ src/katas/katacombs/utils/text-to-speech-converter.ts
@@ -1,13 +1,17 @@
 import { TextToSpeechService } from '@katas/katacombs/utils';
 import { existsSync } from 'node:fs';
+import { getAbsolutePath, RESOURCES_PATH } from '@katas/katacombs/paths';
+import path from 'node:path';
 
 export class TextToSpeechConverter {
     constructor(private readonly service: TextToSpeechService) {}
 
     public async convert(texts: Record<string, string>): Promise<void> {
         for (const [fileName, text] of Object.entries(texts)) {
-            if (!existsSync(fileName)) {
-                await this.service.convert(text, fileName);
+            const absolutePath = getAbsolutePath(path.join(RESOURCES_PATH, 'audio', `${fileName}.mp3`));
+            if (!existsSync(absolutePath)) {
+                await this.service.convert(text, absolutePath);
+                // break;
             }
         }
     }
