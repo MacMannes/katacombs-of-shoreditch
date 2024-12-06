71e63b63761b7e43a45fcb227da2acc6c7a7103a
André Mathlener <info@macmannes.nl>
2024-12-06 21:04:43 +0100

    Added convert-tts script

---
+++ src/katas/katacombs/convert-text-to-speech.ts
@@ -0,0 +1,8 @@
+import { YamlDataLoader } from '@katas/katacombs/domain';
+import { getAbsolutePath, RESOURCES_PATH, TEST_GAME_DATA } from '@katas/katacombs/paths';
+import path from 'node:path';
+import { TextToSpeechConverter, TextToSpeechService } from '@katas/katacombs/utils';
+
+const dataLoader = new YamlDataLoader();
+const realm = await dataLoader.load(getAbsolutePath(path.join(RESOURCES_PATH, TEST_GAME_DATA)));
+await new TextToSpeechConverter(new TextToSpeechService()).convert(realm.texts);
--- src/katas/katacombs/utils/text-to-speech-service.ts
+++ src/katas/katacombs/utils/text-to-speech-service.ts
@@ -1,3 +1,5 @@
 export class TextToSpeechService {
-    public async convert(text: string, filePath: string): Promise<void> {}
+    public async convert(text: string, filePath: string): Promise<void> {
+        console.log(`Converting ${filePath}`);
+    }
 }
