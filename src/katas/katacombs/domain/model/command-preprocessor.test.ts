8e7d9a7b5010a04f8c4b095c221fcd871e405311
André Mathlener <info@macmannes.nl>
2024-12-15 21:07:38 +0100

    Implemented preprocessing command go n, go e etc

--- src/katas/katacombs/domain/model/command-preprocessor.test.ts
+++ src/katas/katacombs/domain/model/command-preprocessor.test.ts
@@ -8,11 +8,15 @@ describe('CommandPreprocessor', () => {
         ['look north', 'look north'],
         ['print invoice', 'print invoice'],
         ['e', 'go east'],
+        ['go e', 'go east'],
         ['w', 'go west'],
+        ['go w', 'go west'],
         ['n', 'go north'],
         ['s', 'go south'],
+        ['go s', 'go south'],
         ['d', 'go down'],
         ['u', 'go up'],
+        ['go u', 'go up'],
     ])(`Processing input "%s" should return "%s"`, (input: string, expected: string) => {
         const result = preprocessor.process(input);
 
--- src/katas/katacombs/domain/model/command-preprocessor.ts
+++ src/katas/katacombs/domain/model/command-preprocessor.ts
@@ -2,16 +2,22 @@ export class CommandPreprocessor {
     public process(input: string): string {
         switch (input) {
             case 'e':
+            case 'go e':
                 return 'go east';
             case 'w':
+            case 'go w':
                 return 'go west';
             case 'n':
+            case 'go n':
                 return 'go north';
             case 's':
+            case 'go s':
                 return 'go south';
             case 'd':
+            case 'go d':
                 return 'go down';
             case 'u':
+            case 'go u':
                 return 'go up';
             default:
                 return input;
