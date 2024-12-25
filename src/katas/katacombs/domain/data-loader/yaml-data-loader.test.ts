38de440ba8948c2707bae5baa4ede73dc4c74e5a
André Mathlener <info@macmannes.nl>
2024-12-25 12:37:13 +0100

    Group some tests in Yaml DataLoader tests

--- src/katas/katacombs/domain/data-loader/yaml-data-loader.test.ts
+++ src/katas/katacombs/domain/data-loader/yaml-data-loader.test.ts
@@ -12,177 +12,183 @@ describe('YamlDataLoader', () => {
         realm = await loader.load(gameDataPath);
     });
 
-    it('should load the rooms', async () => {
-        const rooms = realm.rooms;
-        expect(rooms.length).toBeGreaterThan(2);
-        expect(rooms.find((room) => room.name === 'nowhere')).toBeDefined();
-        expect(rooms.find((room) => room.name === 'start')).toBeDefined();
-        expect(rooms.find((room) => room.name === 'building')).toBeDefined();
+    describe('Rooms', () => {
+        it('should load the rooms', async () => {
+            const rooms = realm.rooms;
+            expect(rooms.length).toBeGreaterThan(2);
+            expect(rooms.find((room) => room.name === 'nowhere')).toBeDefined();
+            expect(rooms.find((room) => room.name === 'start')).toBeDefined();
+            expect(rooms.find((room) => room.name === 'building')).toBeDefined();
+        });
+
+        it('should load short descriptions of the rooms', async () => {
+            const rooms = realm.rooms;
+            expect(rooms.find((room) => room.name === 'nowhere')?.shortDescription).toBeUndefined();
+            expect(rooms.find((room) => room.name === 'start')?.shortDescription).toBeDefined();
+        });
+
+        it('should add connections to  the rooms', async () => {
+            const rooms = realm.rooms;
+            const fromStartToBuilding = rooms.find((room) => room.name === 'start')?.findConnection('north');
+            expect(fromStartToBuilding).toBeDefined();
+            expect(fromStartToBuilding?.description).toBeDefined();
+            expect(fromStartToBuilding?.matchesDirection('inside')).toBeTruthy();
+
+            const fromBuildingToStart = rooms.find((room) => room.name === 'building')?.findConnection('south');
+            expect(fromBuildingToStart).toBeDefined();
+        });
+
+        it('should add items to the rooms', async () => {
+            const rooms = realm.rooms;
+            const building = rooms.find((room) => room.name === 'building');
+            expect(building).toBeDefined();
+
+            const lamp = building?.findItem('lamp');
+            expect(lamp).toBeDefined();
+            expect(lamp?.getDescription('room')).toContain('item-lantern-room');
+            expect(lamp?.getDescription('inventory')).toContain('item-lantern-inventory');
+            expect(lamp?.getDescription('look')).toContain('item-lantern-look');
+        });
     });
 
-    it('should load short descriptions of the rooms', async () => {
-        const rooms = realm.rooms;
-        expect(rooms.find((room) => room.name === 'nowhere')?.shortDescription).toBeUndefined();
-        expect(rooms.find((room) => room.name === 'start')?.shortDescription).toBeDefined();
-    });
+    describe('Items', () => {
+        it('should handle invisible items as expected', async () => {
+            const rooms = realm.rooms;
+            const building = rooms.find((room) => room.name === 'building');
+            expect(building).toBeDefined();
 
-    it('should add connections to  the rooms', async () => {
-        const rooms = realm.rooms;
-        const fromStartToBuilding = rooms.find((room) => room.name === 'start')?.findConnection('north');
-        expect(fromStartToBuilding).toBeDefined();
-        expect(fromStartToBuilding?.description).toBeDefined();
-        expect(fromStartToBuilding?.matchesDirection('inside')).toBeTruthy();
+            const coin = building?.findItem('coin', true);
+            expect(coin).toBeDefined();
+            expect(coin?.isVisible()).toBeFalsy();
+        });
 
-        const fromBuildingToStart = rooms.find((room) => room.name === 'building')?.findConnection('south');
-        expect(fromBuildingToStart).toBeDefined();
-    });
+        it('should handle immovable items as expected', async () => {
+            const rooms = realm.rooms;
+            const building = rooms.find((room) => room.name === 'building');
+            expect(building).toBeDefined();
 
-    it('should add items to the rooms', async () => {
-        const rooms = realm.rooms;
-        const building = rooms.find((room) => room.name === 'building');
-        expect(building).toBeDefined();
+            const desk = building?.findItem('desk', true);
+            expect(desk).toBeDefined();
+            expect(desk?.immovable).toBeTruthy();
 
-        const lamp = building?.findItem('lamp');
-        expect(lamp).toBeDefined();
-        expect(lamp?.getDescription('room')).toContain('item-lantern-room');
-        expect(lamp?.getDescription('inventory')).toContain('item-lantern-inventory');
-        expect(lamp?.getDescription('look')).toContain('item-lantern-look');
-    });
+            const coin = building?.findItem('coin', true);
+            expect(coin).toBeDefined();
+            expect(coin?.immovable).toBeFalsy();
+        });
 
-    it('should add invisible items to the rooms', async () => {
-        const rooms = realm.rooms;
-        const building = rooms.find((room) => room.name === 'building');
-        expect(building).toBeDefined();
+        it('should handle Countable items as expected', async () => {
+            const rooms = realm.rooms;
+            const startRoom = rooms.find((room) => room.name === 'start');
+            expect(startRoom).toBeDefined();
 
-        const coin = building?.findItem('coin', true);
-        expect(coin).toBeDefined();
-        expect(coin?.isVisible()).toBeFalsy();
-    });
+            const coins = startRoom?.findItem('coin', true);
+            expect(coins).toBeDefined();
+            expect(coins instanceof CountableItem).toBeTruthy();
+            expect((coins as unknown as CountableItem).getCount()).toBe(2);
+            expect(coins?.getDescription('room')).toContain('item-coin-room-few');
+        });
 
-    it('should add immovable items to the rooms', async () => {
-        const rooms = realm.rooms;
-        const building = rooms.find((room) => room.name === 'building');
-        expect(building).toBeDefined();
+        it('should add states to the items', async () => {
+            const rooms = realm.rooms;
+            const building = rooms.find((room) => room.name === 'building');
+            expect(building).toBeDefined();
 
-        const desk = building?.findItem('desk', true);
-        expect(desk).toBeDefined();
-        expect(desk?.immovable).toBeTruthy();
+            const lamp = building?.findItem('lamp');
+            expect(lamp).toBeDefined();
+            expect(lamp?.getCurrentState()).toBe('unlit');
 
-        const coin = building?.findItem('coin', true);
-        expect(coin).toBeDefined();
-        expect(coin?.immovable).toBeFalsy();
-    });
+            lamp?.setState('lit');
+            expect(lamp?.getCurrentState()).toBe('lit');
+        });
 
-    it('should add Countable items to the rooms', async () => {
-        const rooms = realm.rooms;
-        const startRoom = rooms.find((room) => room.name === 'start');
-        expect(startRoom).toBeDefined();
+        it('should add triggers to the items', async () => {
+            const rooms = realm.rooms;
+            const building = rooms.find((room) => room.name === 'building');
+            expect(building).toBeDefined();
 
-        const coins = startRoom?.findItem('coin', true);
-        expect(coins).toBeDefined();
-        expect(coins instanceof CountableItem).toBeTruthy();
-        expect((coins as unknown as CountableItem).getCount()).toBe(2);
-        expect(coins?.getDescription('room')).toContain('item-coin-room-few');
-    });
+            const casks = building?.findItem('casks', true);
 
-    it('should add triggers to the items', async () => {
-        const rooms = realm.rooms;
-        const building = rooms.find((room) => room.name === 'building');
-        expect(building).toBeDefined();
-
-        const casks = building?.findItem('casks', true);
-
-        expect(casks).toBeDefined();
-        expect(casks?.triggers).toHaveLength(1);
-        expect(casks?.triggers?.[0]).toStrictEqual({
-            verb: 'look',
-            conditions: undefined,
-            actions: [
-                {
-                    command: 'reveal',
-                    argument: 'coin',
-                    parameter: undefined,
-                    responses: {
-                        success: 'look-casks-success',
-                        failure: 'look-casks-failure',
+            expect(casks).toBeDefined();
+            expect(casks?.triggers).toHaveLength(1);
+            expect(casks?.triggers?.[0]).toStrictEqual({
+                verb: 'look',
+                conditions: undefined,
+                actions: [
+                    {
+                        command: 'reveal',
+                        argument: 'coin',
+                        parameter: undefined,
+                        responses: {
+                            success: 'look-casks-success',
+                            failure: 'look-casks-failure',
+                        },
                     },
-                },
-            ],
+                ],
+            });
+        });
+
+        it('should not add conditions to the triggers for "drop lamp"', async () => {
+            const rooms = realm.rooms;
+            const building = rooms.find((room) => room.name === 'building');
+            expect(building).toBeDefined();
+
+            const lamp = building?.findItem('lantern', true);
+            expect(lamp).toBeDefined();
+            const dropTrigger = lamp?.triggers?.find((trigger) => trigger.verb === 'drop');
+
+            expect(dropTrigger).toBeDefined();
+            expect(dropTrigger?.verb).toBe('drop');
+            expect(dropTrigger?.conditions).toBeUndefined();
+        });
+
+        it('should add conditions to the triggers for "drop cheese"', async () => {
+            const rooms = realm.rooms;
+            const start = rooms.find((room) => room.name === 'start');
+            expect(start).toBeDefined();
+
+            const cheese = start?.findItem('cheese', true);
+            expect(cheese).toBeDefined();
+            expect(cheese?.triggers).toHaveLength(2);
+            const dropTrigger = cheese?.triggers?.[1];
+
+            expect(dropTrigger).toBeDefined();
+            expect(dropTrigger?.verb).toBe('drop');
+            expect(dropTrigger?.conditions).toHaveLength(1);
+            expect(dropTrigger?.conditions?.[0]).toStrictEqual({
+                type: 'location',
+                key: 'currentLocation',
+                value: 'building',
+            });
+        });
+
+        it('should add conditions to the triggers for "look hole"', async () => {
+            const rooms = realm.rooms;
+            const building = rooms.find((room) => room.name === 'building');
+            expect(building).toBeDefined();
+
+            const hole = building?.findItem('hole', true);
+            expect(hole).toBeDefined();
+            expect(hole?.triggers).toHaveLength(1);
+            const lookTrigger = hole?.triggers?.[0];
+
+            expect(lookTrigger).toBeDefined();
+            expect(lookTrigger?.verb).toBe('look');
+            expect(lookTrigger?.conditions).toHaveLength(1);
+            expect(lookTrigger?.conditions?.[0]).toStrictEqual({
+                type: 'hasState',
+                key: 'hole',
+                value: 'unguarded',
+            });
         });
     });
 
-    it('should not add conditions to the triggers for "drop lamp"', async () => {
-        const rooms = realm.rooms;
-        const building = rooms.find((room) => room.name === 'building');
-        expect(building).toBeDefined();
-
-        const lamp = building?.findItem('lantern', true);
-        expect(lamp).toBeDefined();
-        const dropTrigger = lamp?.triggers?.find((trigger) => trigger.verb === 'drop');
-
-        expect(dropTrigger).toBeDefined();
-        expect(dropTrigger?.verb).toBe('drop');
-        expect(dropTrigger?.conditions).toBeUndefined();
-    });
-
-    it('should add conditions to the triggers for "drop cheese"', async () => {
-        const rooms = realm.rooms;
-        const start = rooms.find((room) => room.name === 'start');
-        expect(start).toBeDefined();
-
-        const cheese = start?.findItem('cheese', true);
-        expect(cheese).toBeDefined();
-        expect(cheese?.triggers).toHaveLength(2);
-        const dropTrigger = cheese?.triggers?.[1];
-
-        expect(dropTrigger).toBeDefined();
-        expect(dropTrigger?.verb).toBe('drop');
-        expect(dropTrigger?.conditions).toHaveLength(1);
-        expect(dropTrigger?.conditions?.[0]).toStrictEqual({
-            type: 'location',
-            key: 'currentLocation',
-            value: 'building',
+    describe('NPCs', () => {
+        it('should load the npcs', async () => {
+            const npcs = realm.npcs;
+            expect(Object.keys(npcs)).toHaveLength(1);
+            const shopkeeper = npcs[0];
+            expect(shopkeeper.name).toBe('shopkeeper');
+            expect(shopkeeper.greeting).toBe('npc-shopkeeper-welcome');
         });
-    });
-
-    it('should add conditions to the triggers for "look hole"', async () => {
-        const rooms = realm.rooms;
-        const building = rooms.find((room) => room.name === 'building');
-        expect(building).toBeDefined();
-
-        const hole = building?.findItem('hole', true);
-        expect(hole).toBeDefined();
-        expect(hole?.triggers).toHaveLength(1);
-        const lookTrigger = hole?.triggers?.[0];
-
-        expect(lookTrigger).toBeDefined();
-        expect(lookTrigger?.verb).toBe('look');
-        expect(lookTrigger?.conditions).toHaveLength(1);
-        expect(lookTrigger?.conditions?.[0]).toStrictEqual({
-            type: 'hasState',
-            key: 'hole',
-            value: 'unguarded',
-        });
-    });
-
-    it('should add states to the items', async () => {
-        const rooms = realm.rooms;
-        const building = rooms.find((room) => room.name === 'building');
-        expect(building).toBeDefined();
-
-        const lamp = building?.findItem('lamp');
-        expect(lamp).toBeDefined();
-        expect(lamp?.getCurrentState()).toBe('unlit');
-
-        lamp?.setState('lit');
-        expect(lamp?.getCurrentState()).toBe('lit');
-    });
-
-    it('should load the npcs', async () => {
-        const npcs = realm.npcs;
-        expect(Object.keys(npcs)).toHaveLength(1);
-        const shopkeeper = npcs[0];
-        expect(shopkeeper.name).toBe('shopkeeper');
-        expect(shopkeeper.greeting).toBe('npc-shopkeeper-welcome');
     });
 });
