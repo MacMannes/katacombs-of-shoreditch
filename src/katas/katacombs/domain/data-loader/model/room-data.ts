59241013fbb7dd0ded59a573eb7dffa10b0df831
André Mathlener <info@macmannes.nl>
2024-12-26 11:57:32 +0100

    Use more kebab-case in yaml

--- src/katas/katacombs/domain/data-loader/model/room-data.ts
+++ src/katas/katacombs/domain/data-loader/model/room-data.ts
@@ -4,13 +4,13 @@ export type RoomData = {
     name: string;
     title: string;
     description: string;
-    shortDescription?: string;
+    'short-description'?: string;
     connections?: ConnectionData[];
     items?: ItemData[];
 };
 
 export function toRoom(roomData: RoomData, globalItems: ItemData[]): Room {
-    const room = new Room(roomData.name, roomData.title, roomData.description, roomData.shortDescription);
+    const room = new Room(roomData.name, roomData.title, roomData.description, roomData['short-description']);
 
     room.addConnections(toConnections(roomData.connections));
     room.addItems(toItems(globalItems, roomData.items));
